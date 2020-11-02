33ee7664af51a0a91b1899640a11a2a8be675d2c
Danny <dtfocus1025@gmail.com>
2020-11-01 21:20:55 -0800

    added form control on landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -25,12 +25,14 @@ class LandingPage extends React.Component {
     render() {
         return(
             <div style={{width: "100%", height: "100%", position: "relative"}}>
-                <div style={{width: "450px", height: "600px", backgroundColor: "rgba(232, 231, 225, 0.7)", margin:"0 auto", borderRadius: "30px", marginTop: "20vh", paddingTop: "50px", color: "white"}}>
+                <div style={{width: "450px", height: "450px", backgroundColor: "rgba(232, 231, 225, 0.7)", margin:"0 auto", borderRadius: "30px", marginTop: "20vh", paddingTop: "50px", color: "white"}}>
                     <h1 style={{fontSize: "45px"}}>Trivia Challenge</h1>
                     <h3 style={{paddingTop: "20px"}}>Enter your username</h3>
-                    <input style={{borderRadius: "20px", width: "250px", height: "30px", textAlign: "center", border: "none", outline: "0", fontSize: "20px"}} onChange={this.handleChange} required/>
-                    <div onClick={this.handleEnter} style={{width: "200px", height: "35px", border: "0px solid black", borderRadius: "20px",  
-                    background: "linear-gradient(to left, #ff9999 0%, #ff99cc 100%)", color: "#fff", margin:"20px auto", fontSize: "25px", cursor: "pointer", fontWeight: "bold", marginTop: "20px"}}>Enter</div>
+                    <form>
+                        <input style={{borderRadius: "20px", width: "250px", height: "30px", textAlign: "center", border: "none", outline: "0", fontSize: "20px"}} onChange={this.handleChange} required/>
+                        <input type="submit" onClick={this.handleEnter} style={{width: "200px", height: "35px", border: "0px solid black", borderRadius: "20px",  outline: "0",
+                        background: "linear-gradient(to left, #ff9999 0%, #ff99cc 100%)", color: "#fff", margin:"40px auto", fontSize: "25px", cursor: "pointer", fontWeight: "bold", display: "block"}}/>
+                    </form>
                 </div>
             </div>
         )
