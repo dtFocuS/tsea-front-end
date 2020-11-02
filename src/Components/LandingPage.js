fcc7c1ab0f064be6124f76cb1e43d9c05cdadc02
Danny <dtfocus1025@gmail.com>
2020-11-01 17:56:07 -0800

    added additional styling on the landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -26,10 +26,11 @@ class LandingPage extends React.Component {
         return(
             <div style={{width: "100%", height: "100%", position: "relative"}}>
                 <div style={{width: "450px", height: "600px", backgroundColor: "rgba(232, 231, 225, 0.7)", margin:"0 auto", borderRadius: "30px", marginTop: "20vh", paddingTop: "50px", color: "white"}}>
-                    <h1>Trivia Challenge</h1>
-                    <h4>Enter your username to continue</h4>
-                    <input style={{borderRadius: "20px", width: "250px", height: "30px", textAlign: "center", border: "none", outline: "0", fontSize: "20px"}} onChange={this.handleChange}></input>
-                    <div onClick={this.handleEnter} style={{width: "200px", height: "35px", border: "0px solid black", borderRadius: "20px",  background: "linear-gradient(to left, #ff9999 0%, #ff99cc 100%)", color: "#fff", margin:"20px auto", fontSize: "25px", cursor: "pointer", }}>Enter</div>
+                    <h1 style={{fontSize: "45px"}}>Trivia Challenge</h1>
+                    <h3 style={{paddingTop: "20px"}}>Enter your username</h3>
+                    <input style={{borderRadius: "20px", width: "250px", height: "30px", textAlign: "center", border: "none", outline: "0", fontSize: "20px"}} onChange={this.handleChange} required/>
+                    <div onClick={this.handleEnter} style={{width: "200px", height: "35px", border: "0px solid black", borderRadius: "20px",  
+                    background: "linear-gradient(to left, #ff9999 0%, #ff99cc 100%)", color: "#fff", margin:"20px auto", fontSize: "25px", cursor: "pointer", fontWeight: "bold", marginTop: "20px"}}>Enter</div>
                 </div>
             </div>
         )
